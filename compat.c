c849df547fb4e139a0ba267b12f9a5829be45d42
Jean-André Santoni <jean.andre.santoni@gmail.com>
2021-08-08 16:49:18 +0700

    Load rom from libretro frontend

--- compat.c
+++ compat.c
@@ -32,18 +32,3 @@ muldiv(long a, long b, long c)
 		v = -v;
 	return v;
 }
-
-int
-readn(int f, void *data, int len)
-{
-	uchar *p, *e;
-
-	p = data;
-	e = p + len;
-	while(p < e){
-		if((len = read(f, p, e - p)) <= 0)
-			break;
-		p += len;
-	}
-	return p - (uchar*)data;
-}

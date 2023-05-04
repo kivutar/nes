fcdff504cb668d8a300b552436fc489b555027f2
Jean-André Santoni <jean.andre.santoni@gmail.com>
2023-05-04 10:13:56 +0200

    Try to glue memory

--- hcdebug.cpp
+++ hcdebug.cpp
@@ -1,149 +1,154 @@
 #include <string.h>
 #include "hcdebug.h"
 #include "libretro.h"
+#include "u.h"
+#include "dat.h"
+
+extern int nprg;
+extern uchar *prg;
+extern uchar mem[32768];
 
 hc_DebuggerIf* debugger = NULL;
 
 extern hc_Memory const main_memory;
 extern hc_Cpu const cpu;
 
-
-class HCDebugContext : public ScriptingContext
-{
-public:
-	HCDebugContext(Debugger* d)
-		: ScriptingContext(d)
-	{}
-protected:
-	void InternalCallMemoryCallback(uint16_t addr, uint8_t& value, CallbackType type) override
-	{
-		hc_Event e;
-		switch (type)
-		{
-		case CallbackType::CpuExec:
-			e.type = HC_EVENT_EXECUTION;
-			e.execution.cpu = &cpu;
-			e.execution.address = addr;
-			break;
-		case CallbackType::CpuWrite:
-		case CallbackType::CpuRead:
-			e.type = HC_EVENT_MEMORY;
-			e.memory.memory = &main_memory;
-			e.memory.address = addr;
-			e.memory.operation = (type == CallbackType::CpuRead)
-				? HC_MEMORY_READ
-				: HC_MEMORY_WRITE;
-			e.memory.value = value;
-			break;
-		default:
-			return;
-		}
+// class HCDebugContext : public ScriptingContext
+// {
+// public:
+// 	HCDebugContext(Debugger* d)
+// 		: ScriptingContext(d)
+// 	{}
+// protected:
+// 	void InternalCallMemoryCallback(uint16_t addr, uint8_t& value, CallbackType type) override
+// 	{
+// 		hc_Event e;
+// 		switch (type)
+// 		{
+// 		case CallbackType::CpuExec:
+// 			e.type = HC_EVENT_EXECUTION;
+// 			e.execution.cpu = &cpu;
+// 			e.execution.address = addr;
+// 			break;
+// 		case CallbackType::CpuWrite:
+// 		case CallbackType::CpuRead:
+// 			e.type = HC_EVENT_MEMORY;
+// 			e.memory.memory = &main_memory;
+// 			e.memory.address = addr;
+// 			e.memory.operation = (type == CallbackType::CpuRead)
+// 				? HC_MEMORY_READ
+// 				: HC_MEMORY_WRITE;
+// 			e.memory.value = value;
+// 			break;
+// 		default:
+// 			return;
+// 		}
 		
-		for (auto& ref : _callbacks[(int)type][addr])
-		{
-			if (debugger->v1.handle_event)
-			{
-				debugger->v1.handle_event(debugger->v1.user_data, ref, &e);
-			}
-		}
-	}
+// 		for (auto& ref : _callbacks[(int)type][addr])
+// 		{
+// 			if (debugger->v1.handle_event)
+// 			{
+// 				debugger->v1.handle_event(debugger->v1.user_data, ref, &e);
+// 			}
+// 		}
+// 	}
 	
-	int InternalCallEventCallback(EventType type) override
-	{
-		return 0;
-	}
+// 	int InternalCallEventCallback(EventType type) override
+// 	{
+// 		return 0;
+// 	}
 
-public:
-	uint64_t GetRegister(unsigned reg)
-	{
-		State state;
-		_console->GetCpu()->GetState(state);
-		switch (reg)
-		{
-		case HC_6502_A:
-			return state.A;
-		case HC_6502_X:
-			return state.X;
-		case HC_6502_Y:
-			return state.Y;
-		case HC_6502_S:
-			return state.PS;
-		case HC_6502_PC:
-			return state.PC;
-		case HC_6502_P:
-			return state.SP;
-		default:
-			return 0;
-		}
-	}
+// public:
+// 	uint64_t GetRegister(unsigned reg)
+// 	{
+// 		State state;
+// 		_console->GetCpu()->GetState(state);
+// 		switch (reg)
+// 		{
+// 		case HC_6502_A:
+// 			return state.A;
+// 		case HC_6502_X:
+// 			return state.X;
+// 		case HC_6502_Y:
+// 			return state.Y;
+// 		case HC_6502_S:
+// 			return state.PS;
+// 		case HC_6502_PC:
+// 			return state.PC;
+// 		case HC_6502_P:
+// 			return state.SP;
+// 		default:
+// 			return 0;
+// 		}
+// 	}
 	
-	void SetRegister(unsigned reg, uint64_t value)
-	{
-		State state;
-		CPU* cpu = _console->GetCpu();
-		cpu->GetState(state);
-		switch (reg)
-		{
-		case HC_6502_A:
-			state.A = value;
-			break;
-		case HC_6502_X:
-			state.X =value;
-			break;
-		case HC_6502_Y:
-			state.Y = value;
-			break;
-		case HC_6502_S:
-			state.PS = value;
-			break;
-		case HC_6502_PC:
-			cpu->SetDebugPC(value);
-			return;
-		case HC_6502_P:
-			state.SP = value;
-			break;
-		}
-		cpu->SetState(state);
-	}
+// 	void SetRegister(unsigned reg, uint64_t value)
+// 	{
+// 		State state;
+// 		CPU* cpu = _console->GetCpu();
+// 		cpu->GetState(state);
+// 		switch (reg)
+// 		{
+// 		case HC_6502_A:
+// 			state.A = value;
+// 			break;
+// 		case HC_6502_X:
+// 			state.X =value;
+// 			break;
+// 		case HC_6502_Y:
+// 			state.Y = value;
+// 			break;
+// 		case HC_6502_S:
+// 			state.PS = value;
+// 			break;
+// 		case HC_6502_PC:
+// 			cpu->SetDebugPC(value);
+// 			return;
+// 		case HC_6502_P:
+// 			state.SP = value;
+// 			break;
+// 		}
+// 		cpu->SetState(state);
+// 	}
 	
-	uint8_t peek(uint64_t address)
-	{
-		return _console->GetMemoryManager()->DebugRead(address);
-	}
+// 	uint8_t peek(uint64_t address)
+// 	{
+// 		return _console->GetMemoryManager()->DebugRead(address);
+// 	}
 	
-	void poke(uint64_t address, uint8_t value)
-	{
-		_console->GetMemoryManager()->DebugWrite(address, value);
-	}
+// 	void poke(uint64_t address, uint8_t value)
+// 	{
+// 		_console->GetMemoryManager()->DebugWrite(address, value);
+// 	}
 	
-	void step()
-	{
-		GetDebugger()->ReadStepContext();
-		GetDebugger()->Step();
-	}
+// 	void step()
+// 	{
+// 		GetDebugger()->ReadStepContext();
+// 		GetDebugger()->Step();
+// 	}
 	
-	void step_over()
-	{
-		GetDebugger()->ReadStepContext();
-		GetDebugger()->StepOver();
-	}
+// 	void step_over()
+// 	{
+// 		GetDebugger()->ReadStepContext();
+// 		GetDebugger()->StepOver();
+// 	}
 	
-	void step_out()
-	{
-		GetDebugger()->ReadStepContext();
-		GetDebugger()->StepOut();
-	}
-};
+// 	void step_out()
+// 	{
+// 		GetDebugger()->ReadStepContext();
+// 		GetDebugger()->StepOut();
+// 	}
+// };
 
-HCDebugContext& get_scripting_context()
-{
-	static shared_ptr<HCDebugContext> context;
-	if (context) return *context;
-	shared_ptr<Debugger> debugger = _console->GetDebugger();
-	context.reset(new HCDebugContext(debugger.get()));
-	debugger->AttachScript(context);
-	return *context;
-}
+// HCDebugContext& get_scripting_context()
+// {
+// 	static shared_ptr<HCDebugContext> context;
+// 	if (context) return *context;
+// 	shared_ptr<Debugger> debugger = _console->GetDebugger();
+// 	context.reset(new HCDebugContext(debugger.get()));
+// 	debugger->AttachScript(context);
+// 	return *context;
+// }
 
 static hc_SubscriptionID breakpoint_id = 0;
 
@@ -163,12 +168,12 @@ hc_Memory const main_memory = {
 
 	/* peek */
 	[](uint64_t address) -> uint8_t {
-		return get_scripting_context().peek(address);
+		return mem[address];
 	},
 
 	/* poke */
 	[](uint64_t address, uint8_t value) -> int {
-		get_scripting_context().poke(address, value);
+		mem[address] = value;
 		return true;
 	},
 };
@@ -184,16 +189,16 @@ hc_Memory prg_rom = {
 
 	/* peek */
 	[](uint64_t address) -> uint8_t {
-		uint8_t* data = _console->GetMapper()->GetPrgRom();
-		return (data && address < _console->GetMapper()->GetMemorySize(DebugMemoryType::PrgRom))
+		uint8_t* data = prg;
+		return (data && address < nprg * PRGSZ)
 			? *(data + address)
 			: 0;
 	},
 
 	/* poke */
 	[](uint64_t address, uint8_t value) -> int {
-		uint8_t* data = _console->GetMapper()->GetPrgRom();
-		if (data && address < _console->GetMapper()->GetMemorySize(DebugMemoryType::PrgRom))
+		uint8_t* data = prg;
+		if (data && address < nprg * PRGSZ)
 		{
 			*data = value;
 			return true;
@@ -213,14 +218,14 @@ hc_Cpu const cpu = {
 	/* break_points, num_break_points */
 	NULL, 0,
 	/* get_register */
-	[](unsigned reg) -> uint64_t {
-		return get_scripting_context().GetRegister(reg);
-	},
-	/* set_register */
-	[](unsigned reg, uint64_t value) -> int {
-		get_scripting_context().SetRegister(reg, value);
-		return true;
-	},
+	// [](unsigned reg) -> uint64_t {
+	// 	return get_scripting_context().GetRegister(reg);
+	// },
+	// /* set_register */
+	// [](unsigned reg, uint64_t value) -> int {
+	// 	get_scripting_context().SetRegister(reg, value);
+	// 	return true;
+	// },
 };
 
 hc_Cpu const* cpus[] = {
@@ -261,7 +266,7 @@ static RETRO_CALLCONV void* hc_set_debugger(hc_DebuggerIf* const debugger_if) {
 
 	// if (_console && _console->GetMapper())
 	// {
-		prg_rom.v1.size = _console->GetMapper()->GetMemorySize(DebugMemoryType::PrgRom);
+		prg_rom.v1.size = nprg * PRGSZ;
 	// }
 
 	return (void*)&nes_system;
